8f372643692fa589032af5ee4c27315fce38256d
Calie Rushton <calielrushton@gmail.com>
2019-01-07 12:27:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,8 +15,8 @@ def first_challenge
     }
   }
 
-  # 'iterate through each level until right one is reached' method:
-
+  # Method 1: 'iterate through each level until right one is reached':
+  
   # contacts.each do |person, info|
   #   if person == "Freddy Mercury" # confirm you have the correct person hash
   #     info.each do |subject, answer|
@@ -27,15 +27,13 @@ def first_challenge
   #   end
   # end
 
-  # 'iterate directly over the Freddy Mercury hash by using an enumerator method on it' method:
+  # Method 2: 'iterate directly over the Freddy Mercury hash by using an enumerator method on it':
+  
   contacts["Freddy Mercury"].each do |subject, answer|
     if subject == :favorite_icecream_flavors # confirm the correct key
       answer.delete_if { |x| x == "strawberry"} # iterate over array of values, perform an action.
     end
   end
-
-
-
 
   #remember to return your newly altered contacts hash!
   contacts
